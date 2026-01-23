ccdd1f776a0f4a25aa10d620c6869b1f036c6aae
jenningsx2 <jenningsx2@winthrop.edu>
2026-01-23 10:14:10 -0500

    tried coding but i found it very hard

--- CSCI271_Assignment1_xavierjennings.java
+++ CSCI271_Assignment1_xavierjennings.java
@@ -11,10 +11,38 @@ public class CSCI271_Assignment1_xavierjennings {
 			while (sc.hasNextLine()){
                         	String line = sc.nextLine();
 				String[] fields = line.split(",");
-				for
-                                //System.out.println(line);
+				String name = fields = line.split(",");
 
+				double assignment_Sum = 0;
+				double test_Sum = 0;
 
+				for (int i = 1; i <= 7; i++){
+					assignments_Sum += Double.parseDouble(fields[i]);
+				}
+
+				for (int i = 8; i <= 14; i++){
+					test_Sum += Double.parseDouble(fields[i]);
+				}
+
+				double assignment_Avg = assignment_Sum / 7;
+				double test_Avg = test_Sum / 7;
+
+				double midterm = Double.parseDouble(fields[15]);
+				double final_Exam = Double.parseDouble(fields[16]);
+
+				double E = 0.4 * final_Exam + 0.2 *midterm + 0.1 * test_Avg
+
+				double G;
+
+				if (E < 60){
+				G = E;
+				}
+				else if ( E < 80) {
+					double W = ((E - 60) / 20) * 0.3;
+					G = (1 - W) * E + W * assignment_Avg;
+				}
+				else {
+					G = 0.4 * final_exam + 0.2 * midterm + 0.1 * test_Avg + 0.3 * assignment_avg
 
 			}
 
